54615804c51e7232909243c60911e4e6353389c7
Breighton <breighton@comcast.net>
2015-05-05 14:53:25 -0400

    First running DQ stub

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var template = require('./routes/template');
 var http = require('http');
 var path = require('path');
 
@@ -38,8 +39,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 //}
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/users', user.list);
 //app.get('/tag', tag.find);
+app.get('/DQ/template', template.fetch);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
