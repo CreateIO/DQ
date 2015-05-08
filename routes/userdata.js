fa7fcca2bb7e1fb43cec2c2a23acfd07b907dca4
Breighton <breighton@comcast.net>
2015-05-08 18:14:05 -0400

    Added new userdata route

---
+++ routes/userdata.js
@@ -0,0 +1,45 @@
+var express     = require('express');
+
+var router = express.Router();
+
+function findVersion (currentVersion, templateJSON ) {
+    var objectVersion = "0.0.0";
+    var objectResult = templateJSON[0].template;    // grab first (oldest) version in template
+    console.log('   Check Version init: ' + objectVersion + ' for current version: ' + currentVersion);
+    for (var ii in templateJSON) {
+        var version = templateJSON[ii].version;
+        console.log('  Located Version: ' + version );
+        // get latest version that is less than or equal to current requested version
+        if (version <= currentVersion && version > objectVersion) {
+            console.log('  Found better Version: ' + version );
+            objectVersion = version;
+            objectResult = templateJSON[ii].template;
+        }
+    }
+    return (objectResult);
+};
+
+/*
+ *  GET data for a specified userdata/propertyID/region/ID
+ */
+exports.fetch = function(req, res){
+  console.log("Running fetch for specified userdata:");
+  var resource = req.query.propertyID;
+  var version = req.query.version;
+  console.log(req.query);
+  console.log("   requested resource: " + resource );
+  var resourceFile = '../DQMatchSets/template/' + resource + '.json';
+  fs.readFile(resourceFile, 'utf8', function(err,data) {
+    if (err) {
+        console.log('An error occurred while fetching DQ template resource ' + resource + ' with status: ' + err);
+        res.status(404).send('Resource not found: ' + resourceFile);
+    }
+    var jsonData = JSON.parse(data);
+    console.log(jsonData);
+    var resultObject = findVersion(version, jsonData.versions);
+//    console.log(resultObject);
+    res.send(resultObject);
+  });
+
+};
+
