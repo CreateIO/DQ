94ab2bcbbcc84e7b4d40915bfad711d04b5906f6
Breighton <breighton@comcast.net>
2015-05-08 10:35:53 -0400

    Updated DQ to read templates from DQMatchSets storage and find latest version that matches request

--- routes/template.js
+++ routes/template.js
@@ -1,143 +1,18 @@
-var express = require('express');
+var express     = require('express'),
+    fs          = require('fs');;
+
 var router = express.Router();
-
-// DEFAULT template if no region or ID provided...
-var defaultChartTemplate =
-{ "charts": [{
-  "version": "1.0.0",
-  "template": {
-    "property": [
-      "./scripts/charts/detailCharts.json",
-      "property"
-    ],
-    "radar": [
-      "./scripts/charts/detailRadarCharts.json",
-      "property"
-    ],
-    "marketData": [
-      "../../scripts/charts/detailMarketData.json",
-      "marketData"
-    ]
-  }
-}]
-};
-
-var defaultTabTemplate =
-{ "tabs": [
-  {
-    "version": "1.0.0",
-    "template": {
-      "tabs": [
-        {
-          "name": "Property",
-          "tabs": [
-            {
-              "name": "Overview",
-              "url": "location.html",
-              "hasCharts": false,
-              "radarChart": true,
-              "deny": ""
-            },
-            {
-              "name": "Building",
-              "url": "building.html",
-              "hasCharts": true,
-              "deny": ""
-            },
-            {
-              "name": "Zoning",
-              "url": "zoning.html",
-              "hasCharts": true,
-              "deny": ""
-            },
-            {
-              "name": "Planning",
-              "url": "planning.html",
-              "hasCharts": true,
-              "deny": ""
-            },
-            {
-              "name": "Transit",
-              "url": "transportation.html",
-              "hasCharts": false,
-              "deny": ""
-            },
-            {
-              "name": "Build It!",
-              "url": "building_definition.html",
-              "hasCharts": false,
-              "deny": "Limited Monthly DC_Public_Access"
-            }
-          ]
-        },
-        {
-          "name": "Market",
-          "tabs": [
-            {
-              "name": "Demographics",
-              "url": "demographics.html",
-              "hasCharts": true,
-              "deny": ""
-            },
-            {
-              "name": "Economy",
-              "url": "economy.html",
-              "hasCharts": false,
-              "deny": ""
-            },
-            {
-              "name": "Apartment",
-              "url": "multi_family.html",
-              "hasCharts": false,
-              "deny": ""
-            },
-            {
-              "name": "Commercial",
-              "url": "office_market.html",
-              "hasCharts": false,
-              "deny": ""
-            }
-          ]
-        },
-        {
-          "name": "Financing",
-          "tabs": [
-            {
-              "name": "Debt",
-              "url": "financing.html",
-              "hasCharts": false,
-              "deny": ""
-            },
-            {
-              "name": "Equity",
-              "url": "equity.html",
-              "hasCharts": false,
-              "deny": ""
-            },
-            {
-              "name": "Incentives",
-              "url": "incentives.html",
-              "hasCharts": false,
-              "deny": ""
-            }
-          ]
-        }
-      ]
-    }
-  }]
-}
-;
 
 function findVersion (currentVersion, templateJSON ) {
     var objectVersion = "0.0.0";
-    var objectResult = null;
-    console.log("   Check Version init: " + objectVersion);
+    var objectResult = templateJSON[0].template;    // grab first (oldest) version in template
+    console.log('   Check Version init: ' + objectVersion + ' for current version: ' + currentVersion);
     for (var ii in templateJSON) {
         var version = templateJSON[ii].version;
-        console.log("  Located Version: " + version );
+        console.log('  Located Version: ' + version );
         // get latest version that is less than or equal to current requested version
-        if (version <= currentVersion && version > 'objectVersion') {
-            console.log("  Found better Version: " + version );
+        if (version <= currentVersion && version > objectVersion) {
+            console.log('  Found better Version: ' + version );
             objectVersion = version;
             objectResult = templateJSON[ii].template;
         }
@@ -154,7 +29,7 @@ exports.fetch = function(req, res){
   var version = req.query.version;
   console.log(req.query);
   console.log("   requested resource: " + resource );
-  if (resource == 'tabs') {
+/*  if (resource == 'tabs') {
       var resultObject = findVersion(version, defaultTabTemplate.tabs);
       res.send(defaultTabTemplate.tabs[0].template);
   }
@@ -165,6 +40,19 @@ exports.fetch = function(req, res){
   else {
     console.log ("Error: requested resource " + resource + " not found!");
   }
+*/
+  var resourceFile = '../DQMatchSets/template/' + resource + '.json';
+  fs.readFile(resourceFile, 'utf8', function(err,data) {
+    if (err) {
+        console.log('An error occurred while fetching DQ template resource ' + resource + ' with status: ' + err);
+        res.status(404).send('Resource not found: ' + resourceFile);
+    }
+    var jsonData = JSON.parse(data);
+    console.log(jsonData);
+    var resultObject = findVersion(version, jsonData.versions);
+//    console.log(resultObject);
+    res.send(resultObject);
+  });
 
 };
 
