51ec2c16934a2c5c6806af25e5032feee3077d33
Breighton <breighton@comcast.net>
2015-05-13 18:11:56 -0400

    Return file names along with assets for getURL

--- routes/docURL.js
+++ routes/docURL.js
@@ -3,8 +3,8 @@ var pg          = require('pg');
 var knoxCopy = require('knox-copy');
 
 var router = express.Router();
-
-getFilesFromS3 = function(folderName) {
+/*
+getFilesFromS3 = function(folderName, type) {
   var fullName = 'phillyvi-test-2/assets/imagesets-/' + folderName
   var client = knoxCopy.createClient({
     key: 'AKIAI2NVER2KEZ67CZFQ',
@@ -12,15 +12,21 @@ getFilesFromS3 = function(folderName) {
     bucket: 'io.create'
   });
 
+  var results = [];
   client.streamKeys({
     // omit the prefix to list the whole bucket
     prefix: fullName
   }).on('data', function(key) {
-    console.log(key);
+    if (key.slice(-3) == type)
+    {
+        console.log(key);
+        results.push(key);
+    }
   });
+ return results;
 
 }
-
+*/
 /*
  *  SELECT data for a specified userdata/propertyID/region/ID
  *      then return the URL to the assets for this propertyID
@@ -28,6 +34,7 @@ getFilesFromS3 = function(folderName) {
 exports.fetch = function(req, res){
   var propertyID = req.query.propertyID;
   var version = req.query.version;
+  var type = req.query.type;
   console.log("Running docURL fetch for specified propertyID: " + propertyID);
   console.log(req.query);
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -63,15 +70,38 @@ exports.fetch = function(req, res){
 
         // After all data is returned, close connection and return results
         query.on('end', function() {
-            client.end();
             console.log('Read ' + rows)
             console.log(results);
+
             for (var ii in results) {
                 var folder = results[ii].assets;
-                console.log('  Located assets: ' + folder );
-                getFilesFromS3(folder);
-            }
-           return res.json(results);
+                if (folder != '') {
+                  console.log('  Located assets: ' + folder );
+                  var fullName = 'phillyvi-test-2/assets/imagesets-/' + folder;
+                  var client = knoxCopy.createClient({
+                    key: 'AKIAI2NVER2KEZ67CZFQ',
+                    secret: '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',
+                    bucket: 'io.create'
+                  });
+                  var files = [];
+                  client.streamKeys({
+                    // omit the prefix to list the whole bucket
+                    prefix: fullName
+                  }).on('data', function(key) {
+                    if (key.slice(-3) == type)
+                    {
+                        console.log(key);
+                        files.push(key);
+                    }
+                  }).on('end', function() {
+                    console.log(files);
+                    results[ii].fileNames = files;
+                    return res.json(results);
+                  });
+                }
+             }
+             return res.json(results);
+//           client.end();
         });
 
         query.on('error', function(error) {
