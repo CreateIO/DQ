c21be4e6167c8ab8002f14d6c2ff44d3a34d8b62
Breighton <breighton@comcast.net>
2015-09-13 11:39:43 -0400

    Change to sandbox tests so they won't fail in prod instances.

--- test/sandbox_tests.sh
+++ test/sandbox_tests.sh
@@ -42,7 +42,7 @@ show_header
 #do_curl "${dq_proto}://${dq_host}/DQ" test_root.json
 #do_curl "${dq_proto}://${dq_host}/DQ/" test_root_dir.json
 
-do_curl "${dq_proto}://${dq_host}/DQ/template?resource=tabs-" test_tabs_generic_novers.json | tee curl_result.txt
+do_curl "${dq_proto}://${dq_host}/DQ/template?resource=tabs-&branch=master" test_tabs_generic_novers.json | tee curl_result.txt
 if !(grep -q '^200' curl_result.txt); then
     echo "Test failed: did not receive '200' status"
 fi
@@ -50,7 +50,7 @@ if !(grep -Fq '{"tabs":[{"id":"navProperty","name":"Property","tabs":[{"id":"nav
     echo "Test failed: test_tabs_generic_novers.json"
 fi
 
-do_curl "${dq_proto}://${dq_host}/DQ/template?resource=tabs-&version=1.0.0" test_tabs_generic.json | tee curl_result.txt
+do_curl "${dq_proto}://${dq_host}/DQ/template?resource=tabs-&version=1.0.0&branch=master" test_tabs_generic.json | tee curl_result.txt
 if !(grep -q '^200' curl_result.txt); then
     echo "Test failed: did not receive '200' status"
 fi
@@ -58,7 +58,7 @@ if !(grep -Fq '{"tabs":[{"id":"navProperty","name":"Property","tabs":[{"id":"nav
     echo "Test failed: test_tabs_generic.json"
 fi
 
-do_curl "${dq_proto}://${dq_host}/DQ/template?resource=tabs-&version=1.0.0&branch=master" test_tabs_branch.json | tee curl_result.txt
+do_curl "${dq_proto}://${dq_host}/DQ/template?resource=tabs-&version=1.0.0&branch=test" test_tabs_branch.json | tee curl_result.txt
 if !(grep -q '^200' curl_result.txt); then
     echo "Test failed: did not receive '200' status"
 fi
@@ -166,7 +166,7 @@ do_curl "${dq_proto}://${dq_host}/DQ/datasource?source_name=airRights&regionID=U
 if !(grep -q '^200' curl_result.txt); then
     echo "Test failed: did not receive '200' status"
 fi
-if !(grep -Fq '[{"source":"OCTO","id":897,"field_name":["airRights","property.airRights","core.airRights"]' test_datasource.json); then
+if !(grep -Fq ',"field_name":["airRights","property.airRights","core.airRights"]' test_datasource.json); then
     echo "Test failed: test_datasource.json"
 fi
 
